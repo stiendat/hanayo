4c8d61bf2a67442e88c13c5742380e280106ffcd
Hazuki Onohara <minecraft172zzz@gmail.com>
2019-10-28 22:09:30 +0700

    Fixed the "Signing in..." issue for new players

--- register.go
+++ register.go
@@ -120,8 +120,10 @@ func registerSubmit(c *gin.Context) {
 	}
 	lid, _ := res.LastInsertId()
 
-	db.Exec("INSERT INTO `users_stats`(id, username, user_color, user_style, ranked_score_std, playcount_std, total_score_std, ranked_score_taiko, playcount_taiko, total_score_taiko, ranked_score_ctb, playcount_ctb, total_score_ctb, ranked_score_mania, playcount_mania, total_score_mania) VALUES (?, ?, 'black', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);", lid, username)
-
+	res, err = db.Exec("INSERT INTO `users_stats`(id, username, user_color, user_style, ranked_score_std, playcount_std, total_score_std, ranked_score_taiko, playcount_taiko, total_score_taiko, ranked_score_ctb, playcount_ctb, total_score_ctb, ranked_score_mania, playcount_mania, total_score_mania, ranked_score_std_rx, playcount_std_rx, total_score_std_rx, ranked_score_taiko_rx, playcount_taiko_rx, total_score_taiko_rx, ranked_score_ctb_rx, playcount_ctb_rx, total_score_ctb_rx, ranked_score_mania_rx, playcount_mania_rx, total_score_mania_rx) VALUES (?, ?, 'black', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 ,0, 0, 0, 0, 0, 0, 0);", lid, username)
+	if err != nil {
+		fmt.Println(err)
+	}
 	schiavo.CMs.Send(fmt.Sprintf("User (**%s** | %s) registered from %s", username, c.PostForm("email"), clientIP(c)))
 
 	setYCookie(int(lid), c)
@@ -129,7 +131,7 @@ func registerSubmit(c *gin.Context) {
 
 	rd.Incr("ripple:registered_users")
 
-	addMessage(c, successMessage{T(c, "You have been successfully registered on Ripple! You now need to verify your account.")})
+	addMessage(c, successMessage{T(c, "You have been successfully registered on Ainu! You now need to verify your account.")})
 	getSession(c).Save()
 	c.Redirect(302, "/register/verify?u="+strconv.Itoa(int(lid)))
 }
